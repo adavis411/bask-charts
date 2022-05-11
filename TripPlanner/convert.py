9933810e2799775512c6ff8464b2ff826898b090
joeberkovitz <joseph.berkovitz@gmail.com>
2022-05-11 08:00:26 -0400

    Initial checkin of QGIS project minus the NOAA-raster folder

---
+++ TripPlanner/convert.py
@@ -0,0 +1,41 @@
+# Importing the required libraries
+import xml.etree.ElementTree as Xet
+import pandas as pd
+  
+cols = ["latitude", "longitude", "sid", "title", "chart_title", "type"]
+rows = []
+  
+# Parsing the XML file
+xmlparse = Xet.parse('datapoints.xml')
+root = xmlparse.getroot()
+for i in root:
+    t = i.attrib["station_type"]
+    marker = i.find("marker")
+    latitude = marker.attrib["lat"]
+    longitude = marker.attrib["lng"]
+    title = i.attrib["title"]
+    if "xid" in i.attrib:
+        sid = "x_" + i.attrib["xid"]
+    elif "cid" in i.attrib:
+        sid = "c_" + i.attrib["cid"]
+    elif "tid" in i.attrib:
+        sid = "t_" + i.attrib["tid"]
+    else:
+        sid = ""
+
+    if "chart-title" in i.attrib:
+        chart_title = i.attrib["chart-title"]
+    else:
+        chart_title = ""
+
+    rows.append({"latitude": latitude,
+                 "longitude": longitude,
+                 "sid": sid,
+                 "title": title,
+                 "chart_title": chart_title,
+                 "type": t})
+  
+df = pd.DataFrame(rows, columns=cols)
+  
+# Writing dataframe to csv
+df.to_csv('datapoints.csv', index=False)
